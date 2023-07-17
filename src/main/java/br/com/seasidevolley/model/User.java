e9706405ce128a0a38bb5b05441c7660c14cb272
Mikami <99701339+GuiMikami@users.noreply.github.com>
2023-07-17 18:39:43 -0300

    feat: implementing user model

--- src/main/java/br/com/seasidevolley/model/User.java
+++ src/main/java/br/com/seasidevolley/model/User.java
@@ -1,2 +1,44 @@
-package br.com.seasidevolley.model;public class User {
+package br.com.seasidevolley.model;
+
+public class User {
+
+    private int id;
+    private String cpf;
+    private String email;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+
+
+    public int getId() {
+        return id;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 }
