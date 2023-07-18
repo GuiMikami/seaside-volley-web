2785f7eb3fa95645f70fad5d0cc7bee4f29c2f1b
Mikami <99701339+GuiMikami@users.noreply.github.com>
2023-07-18 19:40:34 -0300

    fix: correcting table creation in database

--- src/main/java/br/com/seasidevolley/dao/UserDAO.java
+++ src/main/java/br/com/seasidevolley/dao/UserDAO.java
@@ -10,26 +10,24 @@ import java.sql.SQLOutput;
 
 public class UserDAO {
 
-    String SQL = "INSERT INTO CLIENTE(ID, CPF, NAME, EMAIL) VALUES (?,?,?,?)";
 
-
-    public void createUser(User user){
-
+    public void createUser(User user) {
+        String SQL = "INSERT INTO USERS(CPF, NAME, EMAIL) VALUES (?,?,?)";
         try {
 
-            Connection con = DriverManager.getConnection("jdbc:h2:~/seasidevolley","sa", "sa");
+            Connection con = DriverManager.getConnection("jdbc:h2:~/seasidevolley", "sa", "sa");
             System.out.println("sucess in connection");
 
             PreparedStatement preparedStatement = con.prepareStatement(SQL);
-            preparedStatement.setInt(1,user.getId());
-            preparedStatement.setString(2,user.getCpf());
-            preparedStatement.setString(3,user.getName());
-            preparedStatement.setString(4,user.getEmail());
+            preparedStatement.setString(1, user.getCpf());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getEmail());
 
             preparedStatement.execute();
             con.close();
 
-        }catch (Exception e){
+        } catch (Exception e) {
+            System.out.println(e);
             System.out.println("fail in connection");
         }
 
